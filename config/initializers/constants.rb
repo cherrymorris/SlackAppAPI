aba546394053c3e0b984f7ac80b86087d9be27d9
ThuHtooSan <cherrymoris@gmail.com>
2024-04-03 12:30:14 +0900

    slackapp api

---
+++ config/initializers/constants.rb
@@ -0,0 +1,18 @@
+# module CONSTANTS
+#   # 400 bad_request
+#   ERR_BAD_REQUEST = '400 Bad Request.'.freeze
+#   # 401 Unauthorized
+#   ERR_UNAUTHORIZED = '401 Unauthorized'.freeze
+
+#   ERR_LOGIN_FAILED = 'login fail'.freeze
+# end
+
+
+module CONSTANTS
+  # 400 bad_request
+  ERR_BAD_REQUEST = '400 Bad Request.'.freeze
+  # 401 Unauthorized
+  ERR_UNAUTHORIZED = '401 Unauthorized'.freeze
+  ERR_LOGIN_FAILED = 'login fail'.freeze
+  SLACK_APP_LINK = 'http://example2_slack.com/member_invite'
+end
